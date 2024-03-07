068a3664dd4cb1c7b9f737433c7b2b0fe175ef79
wolffcode <wolffydev@gmail.com>
2024-03-06 21:45:05 -0600

    Updated responsive version, works on tablets

--- public/js/global.js
+++ public/js/global.js
@@ -1,13 +1,1 @@
-    const hamButton = document.querySelector('#mobile-menu')
-    const menuList = document.querySelector('#mobile-menu-list')
 
-    hamButton.addEventListener('click', toggleMenu)
-
-    function toggleMenu(){
-        const isSideMenuOpen = !menuList.classList.contains('inactive')
-        if(isSideMenuOpen){
-            menuList.classList.add('inactive')
-        }else{
-            menuList.classList.remove('inactive')
-        }
-    }
